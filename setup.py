9150f10dec8dcd61ebad51399f57bdb09b622802
Adrian Rosebrock <jrosebr1@umbc.edu>
2015-05-04 17:16:30 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
 	name = 'imutils',
 	packages = ['imutils'],
-	version = '0.1',
-	description = 'A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, and displaying Matplotlib images easier with OpenCV and Python.',
+	version = '0.2',
+	description = 'A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and Python.',
 	author = 'Adrian Rosebrock',
 	author_email = 'adrian@pyimagesearch.com',
 	url = 'https://github.com/jrosebr1/imutils',
