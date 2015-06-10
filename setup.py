1dfc172a0b39bed1227be675eb5bf062e681af57
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2015-06-10 17:30:09 +0300

    Improved code quality based on PEP-8

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name = 'imutils',
-	packages = ['imutils'],
-	version = '0.2.1',
-	description = 'A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and Python.',
-	author = 'Adrian Rosebrock',
-	author_email = 'adrian@pyimagesearch.com',
-	url = 'https://github.com/jrosebr1/imutils',
-	download_url = 'https://github.com/jrosebr1/imutils/tarball/0.1',
-	keywords = ['computer vision', 'image processing', 'opencv', 'matplotlib'],
-	classifiers = [],
+    name='imutils',
+    packages=['imutils'],
+    version='0.2.1',
+    description='A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and Python.',
+    author='Adrian Rosebrock',
+    author_email='adrian@pyimagesearch.com',
+    url='https://github.com/jrosebr1/imutils',
+    download_url='https://github.com/jrosebr1/imutils/tarball/0.1',
+    keywords=['computer vision', 'image processing', 'opencv', 'matplotlib'],
+    classifiers=[],
 )
