510fe6700cd95b0f08655c7d13496cd53671f497
Adrian Rosebrock <jrosebr1@umbc.edu>
2015-09-10 17:49:00 -0400

    Python 3 support now fully working!

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='imutils',
     packages=['imutils'],
-    version='0.2.4',
-    description='A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and Python.',
+    version='0.3',
+    description='A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and both Python 2.7 and Python 3.',
     author='Adrian Rosebrock',
     author_email='adrian@pyimagesearch.com',
     url='https://github.com/jrosebr1/imutils',
