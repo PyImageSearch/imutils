7cc2522754dbefd8356e0419d1a26de674d4dcde
Adrian Rosebrock <jrosebr1@umbc.edu>
2016-12-18 08:04:16 -0500

    Added `rotate_bound` method

--- imutils/convenience.py
+++ imutils/convenience.py
@@ -29,7 +29,7 @@ def rotate(image, angle, center=None, scale=1.0):
     # if the center is None, initialize it as the center of
     # the image
     if center is None:
-        center = (w / 2, h / 2)
+        center = (w // 2, h // 2)
 
     # perform the rotation
     M = cv2.getRotationMatrix2D(center, angle, scale)
@@ -37,6 +37,30 @@ def rotate(image, angle, center=None, scale=1.0):
 
     # return the rotated image
     return rotated
+
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH))
 
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
