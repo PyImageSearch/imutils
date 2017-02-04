9a1305d6f13c921b7887e801c059d86723c723bd
Vincent Knecht <vincent.knecht@mailoo.org>
2017-02-04 18:07:30 +0100

    Add 'videocapture' argument (default None) to VideoStream and WebcamVideoStream. This allows the user to set capture properties (eg. width, height, fps, etc.) beforehand. In this case, the 'src' argument is ignored.

--- imutils/video/videostream.py
+++ imutils/video/videostream.py
@@ -3,7 +3,7 @@ from .webcamvideostream import WebcamVideoStream
 
 class VideoStream:
 	def __init__(self, src=0, usePiCamera=False, resolution=(320, 240),
-		framerate=32):
+		framerate=32, videocapture=None):
 		# check to see if the picamera module should be used
 		if usePiCamera:
 			# only import the picamera packages unless we are
@@ -20,7 +20,8 @@ class VideoStream:
 		# otherwise, we are using OpenCV so initialize the webcam
 		# stream
 		else:
-			self.stream = WebcamVideoStream(src=src)
+			self.stream = WebcamVideoStream(src=src,
+				videocapture=videocapture)
 
 	def start(self):
 		# start the threaded video stream
