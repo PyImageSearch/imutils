fb08b11259fe229f490b570bcc0032ac95bb1624
Kyle Hounslow <kyle.hounslow@gmail.com>
2017-03-24 17:02:53 -0700

    Add feature to convert python list of numpy images to image grids.

--- imutils/convenience.py
+++ imutils/convenience.py
@@ -173,3 +173,69 @@ def check_opencv_version(major, lib=None):
 def corners_to_keypoints(corners):
     """function to take the corners from cv2.GoodFeaturesToTrack and return cv2.KeyPoints"""
     return [cv2.KeyPoint(kp[0][0], kp[0][1], 1) for kp in corners]
+
+def make_grids_of_images(image_list, image_shape, grid_shape):
+    """
+    ---------------------------------------------------------------------------------------------
+    author: Kyle Hounslow
+    ---------------------------------------------------------------------------------------------
+    Converts a list of single images into a list of 'grid' images of specified rows and columns.
+    A new grid image is started once rows and columns of grid image is filled.
+    Empty space of incomplete grid images are filled with black pixels
+    ---------------------------------------------------------------------------------------------
+    :param image_list: python list of input images
+    :param image_shape: tuple, size each image will be resized to for display
+    :param grid_shape: tuple, shape of image grid (rows, cols)
+    :return: list of grid images in numpy array format
+    ---------------------------------------------------------------------------------------------
+
+    example usage:
+
+    # load single image
+    img = cv2.imread('lena.jpg')
+    # duplicate image 50 times
+    num_imgs = 50
+    img_list = []
+    for i in xrange(num_imgs):
+        img_list.append(img)
+    # convert image list into a grid of 256x256 images tiled in a 5x5 grid
+    grids = make_grids_of_images(img_list, (256, 256), (5, 5))
+    # iterate through grids and display
+    for grid in grids:
+        cv2.imshow('grid image', grid)
+        cv2.waitKey(0)
+
+    ----------------------------------------------------------------------------------------------
+    """
+    if len(image_shape) != 2:
+        raise Exception('image shape must be list or tuple of length 2 (rows, cols)')
+    if len(grid_shape) != 2:
+        raise Exception('grid shape must be list or tuple of length 2 (rows, cols)')
+    image_grids = []
+    # start with black canvas to draw images onto
+    grid_image = np.zeros(shape=(image_shape[1] * (grid_shape[1]), image_shape[0] * grid_shape[0], 3),
+                          dtype=np.uint8)
+    cursor_pos = [0, 0]
+    start_new_img = False
+    for img in image_list:
+        if type(img).__module__ != np.__name__:
+            raise Exception('input of type {} is not a valid numpy array'.format(type(img)))
+        start_new_img = False
+        img = cv2.resize(img, image_shape)
+        # draw image to black canvas
+        grid_image[cursor_pos[1]:cursor_pos[1] + image_shape[1], cursor_pos[0]:cursor_pos[0] + image_shape[0]] = img
+        cursor_pos[0] += image_shape[0]  # increment cursor x position
+        if cursor_pos[0] >= grid_shape[0] * image_shape[0]:
+            cursor_pos[1] += image_shape[1]  # increment cursor y position
+            cursor_pos[0] = 0
+            if cursor_pos[1] >= grid_shape[1] * image_shape[1]:
+                cursor_pos = [0, 0]
+                image_grids.append(grid_image)
+                # reset black canvas
+                grid_image = np.zeros(shape=(image_shape[1] * (grid_shape[1]), image_shape[0] * grid_shape[0], 3),
+                                      dtype=np.uint8)
+                start_new_img = True
+    if start_new_img is False:
+        image_grids.append(grid_image)  # add unfinished grid
+    return image_grids
+
