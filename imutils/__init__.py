c65b4588df7069ff0d8ae0e04070687742e09679
Adrian Rosebrock <jrosebr1@umbc.edu>
2018-03-18 07:25:24 -0400

    Preparing for v0.4.6 relrease

--- imutils/__init__.py
+++ imutils/__init__.py
@@ -2,7 +2,7 @@
 # website:	http://www.pyimagesearch.com
 
 # set the version number
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 
 # import the necessary packages
 from .convenience import translate
--- imutils/face_utils/helpers.py
+++ imutils/face_utils/helpers.py
@@ -29,11 +29,11 @@ def rect_to_bb(rect):
 
 def shape_to_np(shape, dtype="int"):
 	# initialize the list of (x, y)-coordinates
-	coords = np.zeros((68, 2), dtype=dtype)
+	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
 
-	# loop over the 68 facial landmarks and convert them
+	# loop over the facial landmarks and convert them
 	# to a 2-tuple of (x, y)-coordinates
-	for i in range(0, 68):
+	for i in range(0, shape.num_parts):
 		coords[i] = (shape.part(i).x, shape.part(i).y)
 
 	# return the list of (x, y)-coordinates
