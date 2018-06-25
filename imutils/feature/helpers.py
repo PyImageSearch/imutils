78675e55610a4db2bc182f9b9131be5afcfaff1a
Adam Spannbauer <SpannbauerAdam@gmail.com>
2018-06-25 18:06:59 -0400

    fix corners_to_keypoints to always return list instead of None when None input

--- imutils/feature/helpers.py
+++ imutils/feature/helpers.py
@@ -4,7 +4,7 @@ import cv2
 def corners_to_keypoints(corners):
     """function to take the corners from cv2.GoodFeaturesToTrack and return cv2.KeyPoints"""
     if corners is None:
-        keypoints = None
+        keypoints = []
     else:
         keypoints = [cv2.KeyPoint(kp[0][0], kp[0][1], 1) for kp in corners]
 
