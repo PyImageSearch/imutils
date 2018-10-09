aa036f6676d1b82e4b15c41c732f7ffa1cdb034d
mithushancj <mithushancj@gmail.com>
2018-10-09 10:19:19 +0530

    Added Color Extraction function

---
+++ imutils/color_extraction.py
@@ -0,0 +1,127 @@
+import numpy as np
+import cv2
+from sklearn.cluster import KMeans
+from collections import Counter
+
+
+def removeBlack(estimator_labels, estimator_cluster):
+
+    # Check for black
+    hasBlack = False
+
+    # Get the total number of occurance for each color
+    occurance_counter = Counter(estimator_labels)
+
+    # Quick lambda function to compare to lists
+    def compare(x, y): return Counter(x) == Counter(y)
+
+    # Loop through the most common occuring color
+    for x in occurance_counter.most_common(len(estimator_cluster)):
+
+        # Quick List comprehension to convert each of RBG Numbers to int
+        color = [int(i) for i in estimator_cluster[x[0]].tolist()]
+
+        # Check if the color is [0,0,0] that if it is black
+        if compare(color, [0, 0, 0]) == True:
+            # delete the occurance
+            del occurance_counter[x[0]]
+            # remove the cluster
+            hasBlack = True
+            estimator_cluster = np.delete(estimator_cluster, x[0], 0)
+            break
+
+    return (occurance_counter, estimator_cluster, hasBlack)
+
+
+def getColorInformation(estimator_labels, estimator_cluster, hasThresholding=False):
+
+    # Variable to keep count of the occurance of each color predicted
+    occurance_counter = None
+
+    # Output list variable to return
+    colorInformation = []
+
+    # Check for Black
+    hasBlack = False
+
+    # If a mask has be applied, remove th black
+    if hasThresholding == True:
+
+        (occurance, cluster, black) = removeBlack(
+            estimator_labels, estimator_cluster)
+        occurance_counter = occurance
+        estimator_cluster = cluster
+        hasBlack = black
+
+    else:
+        occurance_counter = Counter(estimator_labels)
+
+    # Get the total sum of all the predicted occurances
+    totalOccurance = sum(occurance_counter.values())
+
+    # Loop through all the predicted colors
+    for x in occurance_counter.most_common(len(estimator_cluster)):
+
+        index = (int(x[0]))
+
+        # Quick fix for index out of bound when there is no threshold
+        index = (index-1) if ((hasThresholding & hasBlack)
+                              & (int(index) != 0)) else index
+
+        # Get the color number into a list
+        color = estimator_cluster[index].tolist()
+
+        # Get the percentage of each color
+        color_percentage = (x[1]/totalOccurance)
+
+        # make the dictionay of the information
+        colorInfo = {"cluster_index": index, "color": color,
+                     "color_percentage": color_percentage}
+
+        # Add the dictionary to the list
+        colorInformation.append(colorInfo)
+
+    return colorInformation
+
+
+def extractDominantColor(image, number_of_colors=5, hasThresholding=False):
+
+    # Quick Fix Increase cluster counter to neglect the black(Read Article)
+    if hasThresholding == True:
+        number_of_colors += 1
+
+    # Taking Copy of the image
+    img = image.copy()
+
+    # Convert Image into RGB Colours Space
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Reshape Image
+    img = img.reshape((img.shape[0]*img.shape[1]), 3)
+
+    # Initiate KMeans Object
+    estimator = KMeans(n_clusters=number_of_colors, random_state=0)
+
+    # Fit the image
+    estimator.fit(img)
+
+    # Get Colour Information
+    colorInformation = getColorInformation(
+        estimator.labels_, estimator.cluster_centers_, hasThresholding)
+    return colorInformation
+
+
+def plotColorBar(colorInformation):
+    # Create a 500x100 black image
+    color_bar = np.zeros((100, 500, 3), dtype="uint8")
+
+    top_x = 0
+    for x in colorInformation:
+        bottom_x = top_x + (x["color_percentage"] * color_bar.shape[1])
+
+        color = tuple(map(int, (x['color'])))
+
+        cv2.rectangle(color_bar, (int(top_x), 0),
+                      (int(bottom_x), color_bar.shape[0]), color, -1)
+        top_x = bottom_x
+    return color_bar
