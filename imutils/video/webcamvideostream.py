7c3d33197250292aefdebf286cdcc7893ba8b304
Marty Quish <marty.quish@gmail.com>
2018-11-14 22:40:04 -0600

    WebcamVideoStream now releases its video device when stopped.

--- imutils/video/webcamvideostream.py
+++ imutils/video/webcamvideostream.py
@@ -7,6 +7,12 @@ class WebcamVideoStream:
 		# initialize the video camera stream and read the first frame
 		# from the stream
 		self.stream = cv2.VideoCapture(src)
+		
+		# Keep track of the source of the video capture.
+		# This way, when the user calls stop() we can release the camera.
+		# If the user calls start later, we can simply re-open the video capture.
+		self.src = src
+
 		(self.grabbed, self.frame) = self.stream.read()
 
 		# initialize the thread name
@@ -17,6 +23,10 @@ class WebcamVideoStream:
 		self.stopped = False
 
 	def start(self):
+		# If the stream was closed, re-open it before starting the update thread.
+		if self.isOpened() is False:
+			self.stream = cv2.VideoCapture(self.src)
+			self.stopped=False
 		# start the thread to read frames from the video stream
 		t = Thread(target=self.update, name=self.name, args=())
 		t.daemon = True
@@ -28,6 +38,8 @@ class WebcamVideoStream:
 		while True:
 			# if the thread indicator variable is set, stop the thread
 			if self.stopped:
+				# When the user stops the camera, we should release the resource:
+				self.stream.release()
 				return
 
 			# otherwise, read the next frame from the stream
