5aae9887df3dcada5f8d8fa6af0df2122ad7aaca
Adrian Roseborck <jrosebr1@umbc.edu>
2018-12-03 07:37:01 -0500

    Created imutils.grab_contours function to help manage return signature of cv2.findContours between OpenCV v2.4, v3, and v4

--- imutils/__init__.py
+++ imutils/__init__.py
@@ -13,6 +13,7 @@ from .convenience import skeletonize
 from .convenience import opencv2matplotlib
 from .convenience import url_to_image
 from .convenience import auto_canny
+from .convenience import grab_contours
 from .convenience import is_cv2
 from .convenience import is_cv3
 from .convenience import is_cv4
--- imutils/convenience.py
+++ imutils/convenience.py
@@ -151,6 +151,29 @@ def auto_canny(image, sigma=0.33):
     # return the edged image
     return edged
 
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+            "otherwise OpenCV changed their cv2.findContours return "
+            "signature yet again. Refer to OpenCV's documentation "
+            "in that case"))
+
+    # return the actual contours array
+    return cnts
+
 def is_cv2(or_better=False):
     # grab the OpenCV major version number
     major = get_opencv_major_version()
@@ -162,7 +185,7 @@ def is_cv2(or_better=False):
     # otherwise we want to check for *strictly* OpenCV 2
     return major == 2
 
-def is_cv3(or_better=True):
+def is_cv3(or_better=False):
     # grab the OpenCV major version number
     major = get_opencv_major_version()
 
@@ -173,7 +196,7 @@ def is_cv3(or_better=True):
     # otherwise we want to check for *strictly* OpenCV 3
     return major == 3
 
-def is_cv4(or_better=True):
+def is_cv4(or_better=False):
     # grab the OpenCV major version number
     major = get_opencv_major_version()
 
