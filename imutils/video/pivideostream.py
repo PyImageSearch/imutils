3ac97c3fd38de61a10fc049fa428ead8fa10ac71
Abhishek <abhi.una12@gmail.com>
2019-03-08 22:51:37 +0530

    Major PiVideoStream Fixes and Thread handling.

--- imutils/video/pivideostream.py
+++ imutils/video/pivideostream.py
@@ -2,45 +2,54 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
+import logging
 import cv2
 
 class PiVideoStream:
-	def __init__(self, resolution=(320, 240), framerate=32):
+	def __init__(self, resolution=(320, 240), framerate=30, **options):
 		# initialize the camera and stream
 		self.camera = PiCamera()
 		self.camera.resolution = resolution
 		self.camera.framerate = framerate
 		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
+		self.stream = self.camera.capture_continuous(self.rawCapture,format="bgr", use_video_port=True,  **options)
 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
+		#thread intialization
+		self.thread = None
+
+		#enable logging
+		self.logging = False
+
 		self.stopped = False
 
 	def start(self):
 		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, args=())
-		t.daemon = True
-		t.start()
+		self.thread = Thread(target=self.update, args=())
+		self.thread.daemon = True
+		self.thread.start()
 		return self
 
 	def update(self):
 		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
+		try:
+			for stream in self.stream:
 			# grab the frame from the stream and clear the stream in
 			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
-
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
+				if stream is None:
+					self.stopped =True
+				if self.stopped:
+					break
+				self.frame = stream.array
+				self.rawCapture.seek(0)
+				self.rawCapture.truncate()
+		except Exception as e:
+			if self.logging:
+				logging.error(traceback.format_exc())
+			pass
+		# release resource camera resources
+		self.stream.close()
+		self.rawCapture.close()
+		self.camera.close()
 
 	def read(self):
 		# return the frame most recently read
@@ -49,3 +58,8 @@ class PiVideoStream:
 	def stop(self):
 		# indicate that the thread should be stopped
 		self.stopped = True
+		# wait until stream resources are released (producer thread might be still grabbing frame)
+		if self.thread is not None: 
+			self.thread.join()
+			#properly handle thread exit
+
