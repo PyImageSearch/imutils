2c6f0289cc5906683fa8f1d48436e207b82b4e09
Abhishek <abhi.una12@gmail.com>
2019-03-09 00:05:56 +0530

    Fixes

--- imutils/video/__init__.py
+++ imutils/video/__init__.py
@@ -2,5 +2,6 @@
 from .count_frames import count_frames
 from .fps import FPS
 from .videostream import VideoStream
+from .pivideostream import PiVideoStream
 from .webcamvideostream import WebcamVideoStream
 from .filevideostream import FileVideoStream
