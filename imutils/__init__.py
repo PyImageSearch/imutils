df65a77087548b5952431cc8006d66bcca7b1abc
Adrian Roseborck <jrosebr1@umbc.edu>
2019-08-18 07:50:57 -0400

    Preparing for v0.5.3 release.

--- imutils/__init__.py
+++ imutils/__init__.py
@@ -2,7 +2,7 @@
 # website:	http://www.pyimagesearch.com
 
 # set the version number
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 # import the necessary packages
 from .convenience import translate
