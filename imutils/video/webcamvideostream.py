9ef2f59989485d63582fa3617c447aa29295883d
evgenyslab <e.nuger@gmail.com>
2019-09-24 10:26:24 -0400

    added cleaner start/stop for VideoCapture

--- imutils/video/webcamvideostream.py
+++ imutils/video/webcamvideostream.py
@@ -5,22 +5,35 @@ import cv2
 class WebcamVideoStream:
 	def __init__(self, src=0, name="WebcamVideoStream"):
 		# initialize the video camera stream and read the first frame
-		# from the stream
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
-
+		self.src = src
+		# run hidden start function
+		self.__start__()
 		# initialize the thread name
 		self.name = name
 
+	def __start__(self):
+		"""
+        start opencv stream by taking one image
+        :return:
+        """
+		# from the stream
+		self.stream = cv2.VideoCapture(self.src)
+		(self.grabbed, self.frame) = self.stream.read()
 		# initialize the variable used to indicate if the thread should
 		# be stopped
 		self.stopped = False
 
 	def start(self):
+		"""
+        Start streaming thread, auto-open video capture if its been stopped
+        :return:
+        """
+		if self.stopped:
+			self.__start__()
 		# start the thread to read frames from the video stream
-		t = Thread(target=self.update, name=self.name, args=())
-		t.daemon = True
-		t.start()
+		self.t = Thread(target=self.update, name=self.name, args=())
+		self.t.daemon = True
+		self.t.start()
 		return self
 
 	def update(self):
@@ -33,10 +46,13 @@ class WebcamVideoStream:
 			# otherwise, read the next frame from the stream
 			(self.grabbed, self.frame) = self.stream.read()
 
-	def read(self):
-		# return the frame most recently read
-		return self.frame
-
 	def stop(self):
 		# indicate that the thread should be stopped
 		self.stopped = True
+		self.t.join()
+		self.stream.release()
+
+	def __del__(self):
+		# auto release camera when deleting object:
+		self.stream.release()
+
