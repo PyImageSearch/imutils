3b5bfb5cb854319751a76982e82d4f501aff358b
pawelplodzpl <pawel.kapusta@p.lodz.pl>
2019-12-02 22:26:36 +0100

    modified JetsonVideoStream and FPS demo

---
+++ demos/fps_demo_jetson.py
@@ -0,0 +1,54 @@
+# author:    Adrian Rosebrock
+# website:   http://www.pyimagesearch.com
+
+# USAGE
+# BE SURE TO INSTALL 'imutils' PRIOR TO EXECUTING THIS COMMAND
+# python fps_demo.py
+# python fps_demo.py --display 1
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.video import JetsonVideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+
+# created a *threaded *video stream, allow the camera senor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+jvs = JetsonVideoStream().start()
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = jvs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+jvs.stop()
--- imutils/video/jetsonvideostream.py
+++ imutils/video/jetsonvideostream.py
@@ -3,19 +3,48 @@ from threading import Thread
 import cv2
 
 class JetsonVideoStream:
-	def __init__(self, resolution=(1920, 1080), name="JetsonVideoStream"):
+	def __init__(self, outputResolution=(960, 40), frameRate=28, flipMethod=0, exposureTimeInMiliseconds=None, gain=None, name="JetsonVideoStream"):
 		# set up the gstreamer string used to set up the camera on 
 		# the jetson board
-		width = resolution[0]
-		height = resolution[1]
-		cameraString = ('nvcamerasrc ! '
+		# exposureTime - in miliseconds (0,013 to 683)
+		# gain ( 1.000000 to 10.625000)
+
+		width = outputResolution[0]
+		height = outputResolution[1]
+		exposureTimeSting = ''
+		gainString = ''
+		if exposureTimeInMiliseconds is not None:
+			toNanoseconds = exposureTimeInMiliseconds * 1000000
+			exposureTimeSting = ' exposuretimerange="{}" "{}"'.format(toNanoseconds, toNanoseconds)
+		if gain is not None:
+			gainString = ' gainrange="{}" "{}"'.format(gain, gain)
+		# ispdigitalgainrange - unknown parameter (digital gain), maybe important
+		
+		# some more options: http://www.neko.ne.jp/~freewing/raspberry_pi/nvidia_jetson_nano_setup_raspberry_pi_camera_module_v2/
+		awblock = False
+		aelock = False
+		autoWhiteBalanceLockString = ' awblock=true' if awblock is True else ''
+		autoExposureLockString = ' aelock=true' if aelock is True else ''
+
+		whiteBalanceModeString = ' wbmode=0' # 0 - auto (?)
+                       
+		cameraString = ('nvarguscamerasrc{}{} ! '
                			'video/x-raw(memory:NVMM), '
-               			'width=(int)2592, height=(int)1458, '
-               			'format=(string)I420, framerate=(fraction)30/1 ! '
-               			'nvvidconv ! '
+               			'width=(int)3820, height=(int)1848, '
+               			'format=(string)NV12, framerate=(fraction){}/1 ! '
+               			'nvvidconv flip-method=(int){}  ! '
                			'video/x-raw, width=(int){}, height=(int){}, '
                			'format=(string)BGRx ! '
-               			'videoconvert ! appsink').format(width, height)
+               			'videoconvert ! video/x-raw, format=BGR ! appsink').format(gainString, exposureTimeSting, frameRate, flipMethod, width, height)
+		# Original string from pull request:
+		# cameraString = ('nvcamerasrc ! '
+		# 						'video/x-raw(memory:NVMM), '
+		# 						'width=(int)2592, height=(int)1458, '
+		# 						'format=(string)I420, framerate=(fraction)30/1 ! '
+		# 						'nvvidconv ! '
+		# 						'video/x-raw, width=(int){}, height=(int){}, '
+		# 						'format=(string)BGRx ! '
+		# 						'videoconvert ! appsink').format(width, height)
 
 		# initialize the video camera stream using gstreamer and read 
 		# the first frame from the stream
@@ -41,6 +70,7 @@ class JetsonVideoStream:
 		while True:
 			# if the thread indicator variable is set, stop the thread
 			if self.stopped:
+				# self.stream.release() - TODO: only if it is necessary to release after stopping, can prevent resource blocking
 				return
 
 			# otherwise, read the next frame from the stream
