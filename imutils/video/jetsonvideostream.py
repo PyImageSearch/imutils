2eefd200b18dfe0208517de9df4103bf51e97c76
Jetson-JMR <pawel.kapusta@p.lodz.pl>
2019-12-03 14:13:39 +0100

    add params to gstreamer string

--- imutils/video/jetsonvideostream.py
+++ imutils/video/jetsonvideostream.py
@@ -3,39 +3,54 @@ from threading import Thread
 import cv2
 
 class JetsonVideoStream:
-	def __init__(self, outputResolution=(960, 460), frameRate=28, flipMethod=0, exposureTimeInMiliseconds=None, gain=None, name="JetsonVideoStream"):
-		# set up the gstreamer string used to set up the camera on 
-		# the jetson board
+	def __init__(self, outputResolution=(960, 460), frameRate=28, flipMethod=0, 
+						exposureTimeInMiliseconds=None, gain=None, digitalGain=None, whiteBalanceMode=0,
+						name="JetsonVideoStream"):
+		# set up the gstreamer string used to set up the camera on the jetson board
 		# exposureTime - in miliseconds (0.013 to 683)
 		# gain ( 1.000000 to 10.625000)
-
+		captureWidth = 3264
+		captureHeight = 1848
 		width = outputResolution[0]
 		height = outputResolution[1]
-		exposureTimeSting = ''
-		gainString = ''
+		
+		exposureTimeInMiliseconds=2.56
+		exposureTimeString = ''
 		if exposureTimeInMiliseconds is not None:
-			toNanoseconds = exposureTimeInMiliseconds * 1000000
-			exposureTimeSting = ' exposuretimerange="{}" "{}"'.format(toNanoseconds, toNanoseconds)
+			toNanoseconds = int(exposureTimeInMiliseconds * 1000000)
+			exposureTimeString = 'exposuretimerange="%d %d" ' % (toNanoseconds, toNanoseconds)
+
+		gain = 1.5
+		gainString = ''
 		if gain is not None:
-			gainString = ' gainrange="{}" "{}"'.format(gain, gain)
+			gainString = 'gainrange="%.3f %.3f" ' % (gain, gain)
+
 		# ispdigitalgainrange - unknown parameter (digital gain), maybe important
 		
 		# some more options: http://www.neko.ne.jp/~freewing/raspberry_pi/nvidia_jetson_nano_setup_raspberry_pi_camera_module_v2/
 		awblock = False
 		aelock = False
-		autoWhiteBalanceLockString = ' awblock=true' if awblock is True else ''
-		autoExposureLockString = ' aelock=true' if aelock is True else ''
+		autoWhiteBalanceLockString = 'awblock=true ' if awblock is True else ''
+		autoExposureLockString = 'aelock=true ' if aelock is True else ''
 
-		whiteBalanceModeString = ' wbmode=0' # 0 - auto (?)
-                       
-		cameraString = ('nvarguscamerasrc{}{} ! '
+		whiteBalanceModeString = 'wbmode=%d ' % whiteBalanceMode if whiteBalanceMode is not 0 else ''  # 0 - auto (?)
+        
+
+
+		cameraString =	('nvarguscamerasrc %s%s%s! '
                			'video/x-raw(memory:NVMM), '
-               			'width=(int)3820, height=(int)1848, '
-               			'format=(string)NV12, framerate=(fraction){}/1 ! '
-               			'nvvidconv flip-method=(int){}  ! '
-               			'video/x-raw, width=(int){}, height=(int){}, '
+               			'width=(int)%d, height=(int)%d, '
+               			'format=(string)NV12, framerate=(fraction)%d/1 ! '
+               			'nvvidconv flip-method=%d ! '
+               			'video/x-raw, width=(int)%d, height=(int)%d, '
                			'format=(string)BGRx ! '
-               			'videoconvert ! video/x-raw, format=BGR ! appsink').format(gainString, exposureTimeSting, frameRate, flipMethod, width, height)
+               			'videoconvert ! video/x-raw, format=(string)BGR ! appsink ' # OR format=(string)I420
+						'wait-on-eos=false drop=true max-buffers=1 -e -vvv' % (whiteBalanceModeString, gainString, exposureTimeString,
+																			   captureWidth, captureHeight, frameRate, flipMethod, width, height) )
+		
+		
+		print (cameraString)
+
 		# Original string from pull request:
 		# cameraString = ('nvcamerasrc ! '
 		# 						'video/x-raw(memory:NVMM), '
@@ -70,7 +85,7 @@ class JetsonVideoStream:
 		while True:
 			# if the thread indicator variable is set, stop the thread
 			if self.stopped:
-				# self.stream.release() - TODO: only if it is necessary to release after stopping, can prevent resource blocking
+				self.stream.release() # TODO: only if it is necessary to release after stopping, can prevent resource blocking
 				return
 
 			# otherwise, read the next frame from the stream
