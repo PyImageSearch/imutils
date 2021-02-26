9d49a320161afdbee384bf24764fdeb2eb9f0bcb
Pawel Kranzberg <pawel@kranzberg.co>
2021-02-26 12:31:56 +0100

    Update fps.py

--- imutils/video/fps.py
+++ imutils/video/fps.py
@@ -3,20 +3,22 @@ import datetime
 
 class FPS:
     def __init__(self):
-        # store the start time, end time, and total number of frames
-        # that were examined between the start and end intervals
+        """
+        store the start time, end time, and total number of frames
+        that were examined between the start and end intervals
+        """
         self._start = None
         self._end = None
         self._numFrames = 0
         self._pause = False
 
     def start(self):
-        # start the timer
+        """start the timer"""
         self._start = datetime.datetime.now()
         return self
 
     def stop(self):
-        # stop the timer
+        """stop the timer"""
         self._end = datetime.datetime.now()
         self._pause = True
 
@@ -46,3 +48,25 @@ class FPS:
             return self.start()
         elif if_not_paused == 'raise':
             raise ValueError('Timer has not been paused.')
+
+    def update(self):
+        """
+        increment the total number of frames examined during the
+        start and end intervals
+        """
+        if not self._pause:
+            self._numFrames += 1
+
+    def elapsed(self):
+        """
+        return the total number of seconds between the start and
+        end interval
+        """
+        if self._end is None:
+            return (datetime.datetime.now() - self._start).total_seconds()
+        else:
+            return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        """compute the (approximate) frames per second"""
+        return self._numFrames / self.elapsed()
