d1cdb5a7d42192a41bce90e60cdce69eef431d80
lamhoangtung <lamhoangtung.vz@gmail.com>
2022-03-22 11:01:50 +0700

    Add CI release to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='imutils',
+    name='linus_imutils',
     packages=['imutils', 'imutils.video', 'imutils.io', 'imutils.feature', 'imutils.face_utils'],
     version='0.5.4',
     description='A series of convenience functions to make basic image processing functions such as translation, rotation, resizing, skeletonization, displaying Matplotlib images, sorting contours, detecting edges, and much more easier with OpenCV and both Python 2.7 and Python 3.',
     author='Adrian Rosebrock',
     author_email='adrian@pyimagesearch.com',
-    url='https://github.com/jrosebr1/imutils',
-    download_url='https://github.com/jrosebr1/imutils/tarball/0.1',
+    url='https://github.com/lamhoangtung/imutils',
     keywords=['computer vision', 'image processing', 'opencv', 'matplotlib'],
     classifiers=[],
     scripts=['bin/range-detector'],
